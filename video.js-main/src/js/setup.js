3fddd5fb416efb53b6bdc1444ea3d00856eb47b9
rhein1602 <1694077196@qq.com>
2020-12-17 14:58:11 +0800

    Update setup.js

--- video.js-main/src/js/setup.js
+++ video.js-main/src/js/setup.js
@@ -12,11 +12,11 @@ let _windowLoaded = false;
 let videojs;
 
 /**
- * Set up any tags that have a data-setup `attribute` when the player is started.
+ * 在播放机启动时设置具有数据设置“attribute”的任何标记。
  */
 const autoSetup = function() {
 
-  // Protect against breakage in non-browser environments and check global autoSetup option.
+  // 在非浏览器环境中防止损坏，并选中全局自动设置选项。
   if (!Dom.isReal() || videojs.options.autoSetup === false) {
     return;
   }
@@ -26,43 +26,43 @@ const autoSetup = function() {
   const divs = Array.prototype.slice.call(document.getElementsByTagName('video-js'));
   const mediaEls = vids.concat(audios, divs);
 
-  // Check if any media elements exist
+  // 检查是否存在任何媒体元素
   if (mediaEls && mediaEls.length > 0) {
 
     for (let i = 0, e = mediaEls.length; i < e; i++) {
       const mediaEl = mediaEls[i];
 
-      // Check if element exists, has getAttribute func.
+      //检查元素是否存在，是否具有getAttribute 功能。
       if (mediaEl && mediaEl.getAttribute) {
 
-        // Make sure this player hasn't already been set up.
+        // 请确保尚未设置此player。
         if (mediaEl.player === undefined) {
           const options = mediaEl.getAttribute('data-setup');
 
-          // Check if data-setup attr exists.
-          // We only auto-setup if they've added the data-setup attr.
+          // 检查数据设置属性是否存在。
+          // 我们只在他们添加了数据设置属性的情况下自动设置。
           if (options !== null) {
-            // Create new video.js instance.
+            // 新建视频.js实例。
             videojs(mediaEl);
           }
         }
 
-      // If getAttribute isn't defined, we need to wait for the DOM.
+      // 如果没有定义getAttribute，我们需要等待DOM。
       } else {
         autoSetupTimeout(1);
         break;
       }
     }
 
-  // No videos were found, so keep looping unless page is finished loading.
+  // 找不到视频，所以请继续循环，除非页面加载完毕。
   } else if (!_windowLoaded) {
     autoSetupTimeout(1);
   }
 };
 
 /**
- * Wait until the page is loaded before running autoSetup. This will be called in
- * autoSetup if `hasLoaded` returns false.
+ * 在页面运行之前等待autoSetup。这会被叫来的
+* autoSetup if“hasload”返回false。
  *
  * @param {number} wait
  *        How long to wait in ms
@@ -79,7 +79,7 @@ function autoSetupTimeout(wait, vjs) {
 }
 
 /**
- * Used to set the internal tracking of window loaded state to true.
+ * 用于将窗口加载状态的内部跟踪设置为true。
  *
  * @private
  */
@@ -93,10 +93,9 @@ if (Dom.isReal()) {
     setWindowLoaded();
   } else {
     /**
-     * Listen for the load event on window, and set _windowLoaded to true.
+     * 监听window上的load事件，并将 windowload设置为true。
      *
-     * We use a standard event listener here to avoid incrementing the GUID
-     * before any players are created.
+     * 我们在这里使用标准的事件侦听器，以避免在创建任何播放器之前增加GUID。
      *
      * @listens load
      */
@@ -105,7 +104,7 @@ if (Dom.isReal()) {
 }
 
 /**
- * check if the window has been loaded
+ * 检查窗口是否已加载
  * @return {boolean} zzf add
  */
 const hasLoaded = function() {
